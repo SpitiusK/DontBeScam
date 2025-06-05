112855edbad23b60c5f580f7ad844d1e46815535
SpitiusK <exexd10@mail.ru>
2025-06-05 21:50:25 +0500

    добавил achievementsOnThisNode

--- Assets/Scripts/NodeData.cs
+++ Assets/Scripts/NodeData.cs
@@ -13,12 +13,12 @@ public class NodeData : ScriptableObject
     [SerializeField] private Sprite backgroundImage;
     [SerializeField] private ChoiceData firstChoice;
     [SerializeField] private ChoiceData secondChoice;
-
+    private string ahivmentOnTihisNode = null;
     public string StoryText { get => storyText; set => storyText = value; }
     public Sprite BackgroundImage { get => backgroundImage; set => backgroundImage = value; }
     public ChoiceData FirstChoice { get => firstChoice; set => firstChoice = value; }
     public ChoiceData SecondChoice { get => secondChoice; set => secondChoice = value; }
-
+    
     public void SetChoice(ChoiceType type, ChoiceData choice)
     {
         switch (type)
